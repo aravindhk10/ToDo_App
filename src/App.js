83d36ed4e231b26cd5dd6f8b116e012fce261fc4
aravindhk10 <aravindhk10@gmail.com>
2024-07-18 08:53:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react'
 function App() {
+  const [todos, settodos] = useState([])
+  const [todo, settodo] = useState('')
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  let date = new Date()
+  let today = days[date.getDay()]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br/>
+
+        <h2>Whoop, it's {today} 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={todo} onChange={(e) => settodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={() => {
+          if (todo.trim() !== '') {
+            settodos([...todos, { id: Date.now(), text: todo, status: false, deletedstatus:false }])
+            settodo('')
+          }
+        }} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        <h1>Active Tasks</h1>
+        {todos.map((value) => {
+          return (<div className="todo">
+            <div className="left">
+              <input value={value.status} onChange={(e) => {
+                settodos(todos.filter(obj => {
+                  if (obj.id === value.id) {
+                    obj.status = e.target.checked
+                  }
+                  return obj
+                }))
+
+              }} type="checkbox" name="" id="" />
+              <p>{value.text}</p>
+            </div>
+            <div className="right">
+              <i className="fas fa-times" onClick={() => settodos(todos.filter((obj) => {
+                if(obj.id === value.id){
+                  obj.deletedstatus = true
+                }
+                return (obj.id !== value.id)
+              }))}></i>
+            </div>
+
+          </div>)
+        })}
+        <h1>Completed Tasks</h1>
+        {todos.map((value) => {
+          if (value.status) {
+            return (<h2>{value.text}</h2>)
+          }
+          return null
+        })}
+
+      </div>
     </div>
   );
 }
